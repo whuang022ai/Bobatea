621b187a089bdaf3210b221805d92325524b1494
whuang022ai <whuang022.ai@gmail.com>
2022-05-19 18:47:04 +0800

    fix alias install

---
+++ bobatea.sh
@@ -0,0 +1,17 @@
+########################## 
+# Bobatea datatool
+########################## 
+
+alias pca=PCA.py
+alias log=Log.py
+alias cget=ColumnGet.py
+alias cdrop=ColumnDrop.py
+alias csvf=CSVFilter.py
+alias scatter=Scatter.py
+alias t=DataT.py
+alias pair=PairPlot.py
+alias hcluster=HierarchicalCluster.py
+alias kmean=Kmeans.py
+alias index=CSVAddIndex.py
+alias header=CSVAddHeader.py
+alias merge=CSVMerge.py
--- install.sh
+++ install.sh
@@ -1,22 +1,7 @@
 
+#!/usr/bin/env bash
 
-alias='
-\n ########################## 
-\n # Bobatea_datatool
-\n ########################## 
-\n alias pca=PCA.py
-\n alias log=Log.py
-\n alias cget=ColumnGet.py
-\n alias cdrop=ColumnDrop.py
-\n alias csvf=CSVFilter.py
-\n alias scatter=Scatter.py
-\n alias t=DataT.py
-\n alias pair=PairPlot.py
-\n alias hcluster=HierarchicalCluster.py
-\n alias kmean=Kmeans.py
-\n alias index=CSVAddIndex.py
-\n alias header=CSVAddHeader.py
-\n ######################### 
-'
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+alias="source $SCRIPT_DIR/bobatea.sh"
 echo -e $alias >> ~/.bashrc
 source ~/.bashrc
