8e7bf9555a4805461238fb3cf805ad6a6c0545b1
whuang022ai <whuang022.ai@gmail.com>
2022-06-09 19:57:00 +0800

    refactoring log trans

--- bin/Log.py
+++ bin/Log.py
@@ -2,25 +2,37 @@
 # -*- coding: utf-8 -*-
 
 from sklearn.preprocessing import FunctionTransformer
-import sys
 import pandas as pd
 import numpy as np
 from io import StringIO
 import argparse
+import Util
+from Base import Operator
 
-def log_transform(x):
-    return np.log(x + 1)
+class Log_Operator(Operator):
 
-log_transformer = FunctionTransformer(log_transform)    
+    def __init__(self) -> None:
+        super().__init__()
+        self.log_transformer = FunctionTransformer(self.log_transform)  
 
-data = pd.read_csv(sys.stdin, index_col=0)
+    def log_transform(self,x):
+        return np.log(x + 1)
+    
+    def build_argparser(self):
+        self._parser = argparse.ArgumentParser(description='Log transform ')
+        self._parser=Util.add_argument_common(self._parser)
+    
+    def data_in(self):
+        self.data=Util.input(self._parser)
+    
+    def procress(self):
+        self.log_data=self.log_transformer.fit_transform(self.data)
+        self.log_data.fillna(0)
+        self.log_data = self.log_data[ np.isfinite( self.log_data ).all( axis = 1) ]
+   
+    def data_out(self):
+        Util.output(self._parser,self.log_data)
 
-log_data=log_transformer.fit_transform(data)
+if __name__ == '__main__':
 
-log_data.fillna(0)
-log_data = log_data[ np.isfinite( log_data ).all( axis = 1) ]
-
-output = StringIO()
-
-log_data.to_csv(output, header=True, index=True)
-print (output.getvalue(), file = sys.stdout)
+    Log_Operator().run()
