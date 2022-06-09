1a645043284031a66fb4ddad5553def253871853
whuang022ai <whuang022.ai@gmail.com>
2022-06-09 19:53:29 +0800

    refactoring pca

--- bin/PCA.py
+++ bin/PCA.py
@@ -2,41 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from sklearn.decomposition import PCA
-import sys
 import pandas as pd
 import numpy as np
-from io import StringIO
 import argparse
+import Util
+from Base import Operator
 
-parser = argparse.ArgumentParser(description='sklearn PCA ')
-parser.add_argument('-n','--n_components', action='store', dest='n_components',
-                    default=2 ,help='set pca output component number' )
-parser.add_argument('-t','--transform', action='store_true',
-                    dest='input_matrix_transform',
-                    help='input matrix transform (input.T)')
-parser.add_argument('-e','--expr', action='store',
-                    dest='explained_variance_ratio_outputpath',help='set output path of explained variance ratio for each components')
-args = parser.parse_args()
 
-data = pd.read_csv(sys.stdin, index_col=0)
+class PCA_Operator(Operator):
 
-if args.input_matrix_transform:
-    X = data.values.T
-    index_out = data.columns
-else:
-    X = data.values
-    index_out = data.index
+    def __init__(self) -> None:
+        super().__init__()
 
-pca = PCA(n_components=int(args.n_components))
+    def build_argparser(self):
+        self._parser = argparse.ArgumentParser(description='sklearn PCA ')
+        self._parser = Util.add_argument_common(self._parser)
+        self._parser.add_argument('-n', '--n_components', action='store', dest='n_components',
+                                  default=2, help='set pca output component number')
+        self._parser.add_argument('-e', '--expr', action='store',
+                                  dest='explained_variance_ratio_outputpath', help='set output path of explained variance ratio for each components')
+        self._parser.add_argument('-pc', '--pc_prefix', action='store', dest='pc_prefix', default='PC',
+                                  help='set the prefix name of each principal components (defult is \'PC\' , result will be PC1 , PC2 ... etc. )')
 
-X_pca = pca.fit_transform(X)
-output = StringIO()
-new_column = ['PC' + str(i + 1) for i in range(X_pca.shape[1])]
-pd.DataFrame(X_pca, index=index_out, columns=new_column).to_csv(output,
-        header=True, index=True)
-print (output.getvalue(), file = sys.stdout)
+    def data_in(self):
+        self.data = Util.input(self._parser)
 
-if args.explained_variance_ratio_outputpath:
-    np.savetxt(args.explained_variance_ratio_outputpath,
-               pca.explained_variance_ratio_ * 100, delimiter=',',
-               fmt='%f')
+    def procress(self):
+        self.pca = PCA(n_components=int(self._args.n_components))
+        X_pca = self.pca.fit_transform(self.data.values)
+        new_column = [str(self._args.pc_prefix) + str(i + 1)
+                      for i in range(X_pca.shape[1])]
+        self.result = pd.DataFrame(
+            X_pca, index=self.data.index, columns=new_column)
+
+    def data_out(self):
+        Util.output(self._parser, self.result)
+        if self._args.explained_variance_ratio_outputpath:
+            np.savetxt(self._args.explained_variance_ratio_outputpath,
+                       self.pca.explained_variance_ratio_ * 100, delimiter=',',
+                       fmt='%f')
+
+
+if __name__ == '__main__':
+
+    PCA_Operator().run()
