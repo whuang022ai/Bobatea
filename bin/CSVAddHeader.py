33ddec770fe0f7e983c378981edf92ff9b6eaf23
whuang022ai <whuang022.ai@gmail.com>
2022-06-10 22:36:53 +0800

    refactor csv header

--- bin/CSVAddHeader.py
+++ bin/CSVAddHeader.py
@@ -6,13 +6,35 @@ import sys
 import numpy as np
 from io import StringIO
 import argparse
+import Util
+from Base import Operator
 
-parser = argparse.ArgumentParser(description='add header')
-parser.add_argument('--h', action='store', dest='header')
-args = parser.parse_args()
-output = StringIO()
-output.write(args.header)
-output.write("\n")
-for input in sys.stdin.readlines():
-    output.write(input)
-print (output.getvalue(),end="", file = sys.stdout)
+
+class CSVAddHeader_Operator(Operator):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def build_argparser(self):
+        self._parser = argparse.ArgumentParser(description='Add header of CSV')
+        self._parser.add_argument(
+            '--h', action='store', dest='header')
+
+    def data_in(self):
+        pass
+
+    def procress(self):
+        pass
+
+    def data_out(self):
+        output = StringIO()
+        output.write(self._args.header)
+        output.write("\n")
+        for input in sys.stdin.readlines():
+            output.write(input)
+        print(output.getvalue(), end="", file=sys.stdout)
+
+
+if __name__ == '__main__':
+
+    CSVAddHeader_Operator().run()
